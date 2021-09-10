26233beecc8c39b89fe19f04684f52662e156abe
Henrique <riquessan@gmail.com>
2021-09-10 15:45:41 -0300

    Charater Route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Switch, Route } from "react-router-dom";
 import Home from "../pages";
+import Character from "../pages/character";
 
 export function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      <Route path="/character/:id" exact component={Character} />
     </Switch>
   );
 }
