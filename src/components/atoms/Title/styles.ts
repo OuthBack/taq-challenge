21fed3dd84eba283dae3604a105cf0395254ceda
Henrique <riquessan@gmail.com>
2021-09-10 15:44:46 -0300

    Title Fixes

--- src/components/atoms/Title/styles.ts
+++ src/components/atoms/Title/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Title = styled.h5`
+export const Title = styled.h1`
   line-height: 1.55;
   font-size: clamp(23px, 2.2vw, 35px);
   color: ${(props) => props.theme.font.colors.green200};
