80c89f4df556bdd31ca5aba9791cb3c9baac7a57
Henrique <riquessan@gmail.com>
2021-09-10 15:49:34 -0300

    Styles Changes

--- src/components/organisms/CharacterList/styles.ts
+++ src/components/organisms/CharacterList/styles.ts
@@ -1,40 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-  height: 100%;
   padding: 20px;
-  margin-top: 20px;
   transition: ease-in-out 0.5s;
-  background-color: ${(props) => props.theme.colors.grey100};
-  border-radius: 50px 50px 0px 0px;
+  height: 330px;
+  background-color: ${(props) => props.theme.colors.grey700};
 
-  h1.title {
+  h1.big-title {
     margin-top: 20px;
-    line-height: 1.55;
-    font-size: clamp(23px, 2.2vw, 35px);
-  }
-  h5.subtitle {
-    line-height: 1.2;
-    font-size: 14px;
-    color: ${(props) => props.theme.font.colors.blue400};
   }
 
   @media screen and (min-width: 1080px) {
     height: auto;
-    margin-top: 50px;
-
     flex: 1 1 0;
     padding: 40px;
-  }
-
-  @media screen and (min-width: 1366px) {
-    padding: 20px;
-  }
-
-  @media screen and (min-width: 1170px) {
-    h1.title {
-      font-size: 35px;
-    }
   }
 `;
 
@@ -43,24 +22,13 @@ export const List = styled.section`
   display: grid;
   grid-gap: 15px;
   width: 100%;
-  max-height: 100%;
-  overflow: auto;
-
-  ::-webkit-scrollbar {
-    display: none;
-  }
 
   @media screen and (min-width: 768px) {
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr;
   }
 
   @media screen and (min-width: 1080px) {
-    max-height: calc(100vh - 257px);
     padding-right: 20px;
-
-    ::-webkit-scrollbar {
-      display: block;
-    }
   }
 
   @media screen and (min-width: 1280px) {
@@ -68,16 +36,16 @@ export const List = styled.section`
   }
 
   @media screen and (min-width: 1600px) {
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr;
   }
   @media screen and (min-width: 2300px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
   }
 `;
 
 export const CharacterCardSkeleton = styled.div`
   width: 100%;
-  height: 80px;
-  background-color: ${(props) => props.theme.colors.grey600}32;
+  height: 330px;
+  background-color: ${(props) => props.theme.colors.green700};
   border-radius: 20px;
 `;
