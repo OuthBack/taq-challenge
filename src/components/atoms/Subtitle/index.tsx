e37c20d8297a65a1dc0e69f34cc1ef3cad7e8fc8
Henrique <riquessan@gmail.com>
2021-09-10 09:35:33 -0300

    Subtitle

---
+++ src/components/atoms/Subtitle/index.tsx
@@ -0,0 +1,6 @@
+import { IChildren } from "../../../dtos";
+import { SubTitle as H5SubTitle } from "./styles";
+
+export function SubTitle({ children }: IChildren) {
+  return <H5SubTitle className="subtitle">{children}</H5SubTitle>;
+}
---
+++ src/components/atoms/Subtitle/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SubTitle = styled.h5`
+  line-height: 1.2;
+  font-size: 14px;
+  color: ${(props) => props.theme.font.colors.blue400};
+`;
