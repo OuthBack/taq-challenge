1667571b8e4ca8be0d02d0132a2bbf17fff89fcf
Henrique <riquessan@gmail.com>
2021-09-10 09:35:45 -0300

    Home list

--- src/components/templates/HomeList/index.tsx
+++ src/components/templates/HomeList/index.tsx
@@ -1,10 +1,11 @@
-import React from "react";
 import { CharacterList } from "../../organisms/CharacterList";
+import { Logo } from "../../organisms/Logo";
 import { Container } from "./styles";
 
 export function HomeList() {
   return (
     <Container>
+      <Logo />
       <CharacterList />
     </Container>
   );
--- src/components/templates/HomeList/styles.ts
+++ src/components/templates/HomeList/styles.ts
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: black;
+  background-color: ${(props) => props.theme.colors.grey700};
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  img.logo {
+    margin: 0 auto;
+  }
 `;
