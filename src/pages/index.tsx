9ef416688e376bce160138ffab381426d43ec092
Henrique <riquessan@gmail.com>
2021-09-10 15:47:19 -0300

    New Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,38 @@
+import { useQuery, gql } from "@apollo/client";
+import { useCallback, useEffect, useState } from "react";
+import { Error } from "../components/templates/Error";
 import { HomeList } from "../components/templates/HomeList";
+import { ICharacterIDStatus } from "../types";
+
+const getAllCharactersQuery = gql`
+  query {
+    characters {
+      results {
+        name
+        status
+        image
+        id
+      }
+    }
+  }
+`;
 
 export default function Home() {
-  return <HomeList />;
+  const { loading, error, data } = useQuery(getAllCharactersQuery);
+  const [characters, setCharacters] = useState<ICharacterIDStatus[]>([]);
+
+  const getAllCharacters = useCallback(() => {
+    if (!loading) setCharacters(data.characters.results);
+  }, [data, loading]);
+
+  useEffect(() => {
+    getAllCharacters();
+  }, [data, loading, getAllCharacters]);
+
+  if (error) {
+    console.error(error);
+    return <Error>Ops... Ocorreu um erro</Error>;
+  }
+
+  return <HomeList characters={characters} loading={loading} />;
 }
