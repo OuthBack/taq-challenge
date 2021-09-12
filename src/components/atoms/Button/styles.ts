7e80f864b39a28c3522abf49ee5e3741596dbe9e
Henrique <riquessan@gmail.com>
2021-09-12 13:12:19 -0300

    Button Style Updates

--- src/components/atoms/Button/styles.ts
+++ src/components/atoms/Button/styles.ts
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-  background-color: ${(props) => props.theme.colors.green300};
+  background-color: ${(props) => props.theme.colors.grey800};
   border: 1px solid ${(props) => props.theme.font.colors.green700};
   border-radius: 2px;
-  color: ${(props) => props.theme.font.colors.white};
+  color: ${(props) => props.theme.font.colors.blue400};
   font-weight: bold;
   padding: 5px;
   width: 100px;
+  transition: 0.2s ease-in-out;
 
   :hover {
-    background-color: ${(props) => props.theme.colors.green500};
+    background-color: ${(props) => props.theme.colors.green700};
   }
 `;
