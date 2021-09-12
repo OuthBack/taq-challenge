0f00e6c5a0c8791a846854b36462100252faf4de
Henrique <riquessan@gmail.com>
2021-09-12 13:14:52 -0300

    Style Updates

--- src/components/atoms/Subtitle/styles.ts
+++ src/components/atoms/Subtitle/styles.ts
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
-export const SubTitle = styled.h5`
-  line-height: 1.2;
+export const SubTitle = styled.h5<{ color?: string }>`
+  line-height: 1.6;
   font-size: 14px;
   color: ${(props) => props.theme.font.colors.blue400};
+
+  * {
+    color: ${(props) => props.theme.font.colors.green200};
+  }
 `;
